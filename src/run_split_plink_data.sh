eaedde6ffbf05d93ad7e80fe2f4a2b465d546b08
Kody DeGolier <baron063@acn37.agate.msi.umn.edu>
2025-12-05 14:20:53 -0600

    Adjustments to incorporate fixed gwas split

--- src/run_split_plink_data.sh
+++ src/run_split_plink_data.sh
@@ -12,17 +12,17 @@
 
 ### --- CONFIGURABLE DEFAULTS -------------------------------------------------
 # Defaults (can be overridden via CLI)
-plink_file_anc1="/home/gdc/public/prs_methods/data/test/sim_2/AFR_simulation"                     # target ancestry
-plink_file_anc2="/home/gdc/public/prs_methods/data/test/sim_2/EUR_simulation"                     # training ancestry
+plink_file_anc1="/projects/standard/gdc/public/prs_methods/data/test/sim_1/AFR_simulation_study_sample"                     # target ancestry
+plink_file_anc2="/projects/standard/gdc/public/prs_methods/data/test/sim_1/EUR_simulation_study_sample"                     # training ancestry
 
-gwas_percent=40
-train_percent=30
-valid_percent=10
-test_percent=20
+gwas_percent=0
+train_percent=50
+valid_percent=20
+test_percent=30
 rand_seed=42
 no_plink=0
-path_to_repo=/home/gdc/public/prs_methods/scripts/prs_pipeline # place the code is located
-# path_R_packages=/home/gdc/public/Ref/R
+path_to_repo=/projects/standard/gdc/public/prs_methods/scripts/prs_pipeline # place the code is located
+# path_R_packages=/projects/standard/gdc/public/Ref/R
 # export R_LIBS_USER="$path_R_packages"
 
 
@@ -35,16 +35,15 @@ Options:
   -1 <plink_file_anc1>      Full path to target ancestry plink files (default: ${plink_file_anc1})
   -2 <plink_file_anc2>      Full path to training ancestry plink files  (default: ${plink_file_anc2})
   -r <repo_path>            Path to prs_pipeline repo (default: ${path_to_repo})
-  -g <gwas_percent>         Percent of data for GWAS (default: 40)
-  -t <train_percent>        Percent of data for training (default: 30)
+  -t <train_percent>        Percent of data for training (default: 50)
   -T <testing_percent>      Percent of data for testing (default: 20)
-  -v <validation_percent>   Percent of data for validation (default: 10)
+  -v <validation_percent>   Percent of data for validation (default: 30)
   -S <seed>                 Randomization seed (default:42)
   -N <no_plink>             Include flag to skip generating plink separated files (default: set to generate split plink files). 
   -h                        show this help and exit
 
 Example:
-  bash prs_pipeline/src/run_split_plink_data.sh -1 /home/gdc/public/prs_methods/data/test/sim_1/AFR_simulation -2 /home/gdc/public/prs_methods/data/test/sim_1/EUR_simulation -g 40 -t 30 -T 20 -v 10 -S 40
+  bash prs_pipeline/src/run_split_plink_data.sh -1 /projects/standard/gdc/public/prs_methods/data/test/sim_1/AFR_simulation_study_sample -2 /projects/standard/gdc/public/prs_methods/data/test/sim_1/EUR_simulation_study_sample -t 40 -T 30 -v 30 -S 40
 
   Using default settings but skipping generation of separated files
     bash prs_pipeline/src/run_split_plink_data.sh -N
@@ -60,12 +59,11 @@ log() {
 
 
 ### --- PARSE ARGS ------------------------------------------------------------
-while getopts ":1:2:r:g:t:T:v:S:Nh" opt; do
+while getopts ":1:2:r:t:T:v:S:Nh" opt; do
   case "$opt" in
     1) plink_file_anc1="$OPTARG" ;;
     2) plink_file_anc2="$OPTARG" ;;
     r) path_to_repo="$OPTARG" ;;
-    g) gwas_percent="$OPTARG" ;;
     t) train_percent="$OPTARG" ;;
     T) test_percent="$OPTARG" ;;
     v) valid_percent="$OPTARG" ;;
@@ -87,11 +85,10 @@ mkdir -p "${base_location}/samples_training"
 mkdir -p "${base_location}/samples_testing"
 
 # Split phenotype / plink files
-source /home/gdc/public/envs/load_miniconda3.sh
+source /projects/standard/gdc/public/envs/load_miniconda3.sh
 log "Splitting plink data for "$plink_file_anc1""
 if [ "${no_plink}" -eq 1 ]; then
   python "${path_to_repo}/src/split_plink_samples.py" "${plink_file_anc1}" \
-    --gwas "${gwas_percent}" \
     --train "${train_percent}" \
     --val "${valid_percent}" \
     --test "${test_percent}" \
@@ -102,7 +99,6 @@ if [ "${no_plink}" -eq 1 ]; then
   log "Splitting plink data for "$plink_file_anc2""
 
   python "${path_to_repo}/src/split_plink_samples.py" "${plink_file_anc2}" \
-    --gwas "${gwas_percent}" \
     --train "${train_percent}" \
     --val "${valid_percent}" \
     --test "${test_percent}" \
@@ -112,7 +108,6 @@ if [ "${no_plink}" -eq 1 ]; then
 else
 
   python "${path_to_repo}/src/split_plink_samples.py" "${plink_file_anc1}" \
-    --gwas "${gwas_percent}" \
     --train "${train_percent}" \
     --val "${valid_percent}" \
     --test "${test_percent}" \
@@ -122,7 +117,6 @@ else
   log "Splitting plink data for "$plink_file_anc2""
 
   python "${path_to_repo}/src/split_plink_samples.py" "${plink_file_anc2}" \
-    --gwas "${gwas_percent}" \
     --train "${train_percent}" \
     --val "${valid_percent}" \
     --test "${test_percent}" \
@@ -134,8 +128,7 @@ conda deactivate
 conda deactivate
 
 
-echo "Finished generating inputs with the following parameters 
-gwas: ${gwas_percent}%, 
+echo "Finished generating inputs with the following parameters  
 training model: ${train_percent}%,
 validation model: ${valid_percent}%, 
 testing model: ${test_percent}% "
