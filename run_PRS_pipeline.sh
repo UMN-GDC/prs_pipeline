2ccbe828a3e60f03cd30e06dbb1a2bdcd01cda43
Kody DeGolier <baron063@cn1164.agate.msi.umn.edu>
2026-01-29 12:09:18 -0600

    Adjustments to prepare for potential merging

--- run_PRS_pipeline.sh
+++ run_PRS_pipeline.sh
@@ -11,26 +11,18 @@
 # ---- Defaults (documented only; must be overridden or validated) ----
 # Needs the conda environment gdcPipeline
 path_code="/projects/standard/gdc/public/prs_methods/scripts/PRScsx"
-path_data_root="/projects/standard/gdc/public/prs_methods/data/simulated_1000G"
 path_ref_dir="/projects/standard/gdc/public/prs_methods/ref/ref_PRScsx/1kg_ref"
-path_plink2="/projects/standard/gdc/public/plink2"
 anc1="AFR"
 anc2="EUR"
 target_sumstats_file="${path_data_root}/gwas/target_sumstats.txt"
 training_sumstats_file="${path_data_root}/gwas/training_sumstats.txt"
-output_dir="${path_data_root}"
 reference_SNPS_bim="${path_data_root}/anc1_plink_files/${anc1}_simulation_study_sample" # This is the full path except for extension for bim file
-study_sample_plink="${path_data_root}/anc1_plink_files/${anc1}_simulation_study_sample"
 study_sample_plink_anc2="${path_data_root}/anc2_plink_files/${anc2}_simulation_study_sample"
-prs_pipeline="/projects/standard/gdc/public/prs_methods/scripts/prs_pipeline"
+prs_pipeline="/projects/standard/gdc/public/prs_methods/scripts/prs_pipeline" # Path to the prs_pipeline code
 
 ####### Variables ####### # VIPRS
 # Needs the conda environment viprs_env
-path_data=/projects/standard/gdc/public/prs_methods/data/simulated_1000G
-out_path=/projects/standard/gdc/public/prs_methods/data/simulated_1000G
-path_plink2=/projects/standard/gdc/public/plink2
 bfile_gwas_input=${path_data}/anc1_plink_files/archived/AFR_simulation_gwas
-bfile_study_sample=${path_data}/anc1_plink_files/AFR_simulation_study_sample
 covariate_file_gwas=/projects/standard/gdc/public/prs_methods/data/simulated_1000G/prs_pipeline/viprs/gwas/temp/viprs_summary_stats_covar_sex_no_header.txt
 covariate_file_study_sample=${path_data}/prs_pipeline/viprs/study_sample_covar.txt # No header FID IID Sex
 
@@ -39,3 +31,8 @@ path_plink2=/projects/standard/gdc/public/plink2
 path_data=/projects/standard/gdc/public/prs_methods/data/simulated_1000G # AKA path_data_root (prscsx)
 out_path=/projects/standard/gdc/public/prs_methods/data/simulated_1000G # AKA output_dir (prscsx)
 study_sample_plink="${path_data_root}/anc1_plink_files/${anc1}_simulation_study_sample" # AKA bfile_study_sample (prscsx)
+
+# to make cross-compatable
+path_data_root="${path_data}"
+output_dir="${out_path}"
+bfile_study_sample="${study_sample_plink}"
