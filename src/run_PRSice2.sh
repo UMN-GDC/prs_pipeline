4c8e45b2a277cfa60997dc91952fdb4529b878f4
mainsqu33ze <baron063@umn.edu>
2026-02-20 10:05:53 -0600

    inital script creation.

---
+++ src/run_PRSice2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+summary_stats_file=$1
+study_sample=$2
+binary_flag=$3
+phenotype_info_file=$4
+output_path=$5
+
+final_output=${output_path}/prs_pipeline/PRSice2/prs_method
+mkdir -p $final_output
+
+# run PRSice-2 on simulated data--note that this is the temporary data and is unlikely to produce correct results (use this more for formatting purposes)
+Rscript ../src/PRSice.R \
+    --prsice ../src/PRSice_linux \
+    --base ${summary_stats_file} \
+    --target ${study_sample} \
+    --binary-target ${binary_flag} \
+    --pheno ${phenotype_info_file} \
+    --stat beta \
+    --beta \
+    --out ${final_output}/PRSice2_outputs
