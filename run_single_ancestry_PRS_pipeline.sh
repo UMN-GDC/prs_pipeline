ef1fa3b81a39ea9d26fc76f8cd31225de10b1a2a
ood-baron063 <ood-baron063@nbdc-prod-login.general-loris-midb-prod.stratus.msi.umn.edu>
2026-05-12 10:41:00 -0500

    Removal of parallelization

--- run_single_ancestry_PRS_pipeline.sh
+++ run_single_ancestry_PRS_pipeline.sh
@@ -96,7 +96,7 @@ awk '{print $1, $2, $6}' OFS="\t" ${study_sample}.fam > ${output_path}/gwas/stud
 
 # --- METHOD 1: Clumping + Thresholding (C+T) ---
 if [[ "$RUN_CT" == true ]]; then
-    (
+    
     echo "[$(date)] Starting C+T Pipeline..."
     
     # 2. Config & Run
@@ -112,12 +112,12 @@ path_prs_pipeline=${path_repo}
 EOF
 
   bash "${path_repo}/src/run_CT.sh" --c "$CT_CONFIG"
-    ) &
+     
 fi
 
 # --- METHOD 2: LDpred2 ---
 if [[ "$RUN_LDPRED2" == true ]]; then
-    (
+    
     echo "[$(date)] Starting LDpred2 Pipeline..."
     mkdir -p "${output_path}/prs_pipeline/LDpred2"
     echo "Running below
@@ -132,12 +132,12 @@ if [[ "$RUN_LDPRED2" == true ]]; then
         --ss "$summary_stats_file" \
         --bim "$bim_file_path" \
         --out "${output_path}/prs_pipeline/LDpred2/prs_method"
-    ) &
+    
 fi
 
 # --- METHOD 3: lassosum2 ---
 if [[ "$RUN_LASSOSUM2" == true ]]; then
-    (
+    
     echo "[$(date)] Starting lassosum2 Pipeline..."
     mkdir -p "${output_path}/prs_pipeline/lassosum2"
     
@@ -154,13 +154,13 @@ if [[ "$RUN_LASSOSUM2" == true ]]; then
         --ss "$summary_stats_file" \
         --bim "$bim_file_path" \
         --out "${output_path}/prs_pipeline/lassosum2/prs_method"
-    ) &
+    
 fi
 
 
 # --- METHOD 4: PRSice2 ---
 if [[ "$RUN_PRSice2" == true ]]; then
-    (
+    
     echo "[$(date)] Starting PRSice2 Pipeline..."
     mkdir -p "${output_path}/prs_pipeline/PRSice2"
     
@@ -182,7 +182,7 @@ if [[ "$RUN_PRSice2" == true ]]; then
         "${output_path}/gwas/study_sample_pheno.txt" \
         "${output_path}" \
         "${path_repo}"
-    ) &
+    
 fi
 
 echo "All jobs submitted to background. Waiting for completion of all chosen methods."
